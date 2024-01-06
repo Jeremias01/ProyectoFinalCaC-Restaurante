9cd51c7d96a2548849ff90ecc8cc656ae9dfa1ab
Jeremias01 <jere7005@gmail.com>
2024-01-06 20:26:38 -0300

    Agrego la copia de RamaMia por primera vez

---
+++ CAC_trabajo_integrador-RepoGithub/src/main/java/model/Usuario.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Usuario {
+    private String nombreDeUsuario;
+    private String contraseña;
+    }
+
